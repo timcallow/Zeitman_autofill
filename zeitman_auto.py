9e6ea16f0e2eab564f1c4b006c1834497b24b68d
Timothy Callow <timjcallow@gmail.com>
2022-12-22 14:32:55 +0100

    Update for new selenium API

--- zeitman_auto.py
+++ zeitman_auto.py
@@ -8,6 +8,7 @@ Automatically fill out zeitweb form
 import getpass
 import random
 import time
+import os
 
 from selenium.common.exceptions import (
     UnexpectedTagNameException,
@@ -17,35 +18,13 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from webdriver_manager.chrome import ChromeDriverManager
 
+# import config.py file - copy template if doesn't exist
+if not os.path.exists("config.py"):
+    print("config.py file does not exist: copying template!")
+    os.popen("cp config_template.py config.py")
+    time.sleep(1)  # otherwise config is not imported correctly
+import config
 
-###################################################
-
-# user defined parameters
-
-month = 1  # month by number
-year = 2022
-
-# time data
-
-# average start time
-starthr = 9
-startmin = 30
-
-# average finish time
-endhr = 18
-endmin = 30
-
-# random noise (in blocks of 10 minutes)
-rand_start = 1
-rand_end = 3
-
-# whether to overwrite existing presence data
-overwrite_data = False
-
-# whether to automatically save and exit
-save_and_exit = True
-
-##################################################
 
 # user information
 usr = input("Username: ")
@@ -56,17 +35,17 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://zeitweb.hzdr.de/scripts_zeitm/login.php?sso=2")
 
 # login
-usr_elem = driver.find_element_by_name("username")
+usr_elem = driver.find_element("name", "username")
 usr_elem.clear()
 usr_elem.send_keys(usr)
-pswd_elem = driver.find_element_by_name("passwort")
+pswd_elem = driver.find_element("name", "passwort")
 pswd_elem.send_keys(pswd)
-submit = driver.find_element_by_id("button")
+submit = driver.find_element("id", "button")
 submit.click()
 
 # go to chosen month
-monthstr = "jahrmonat(" + str(year) + "," + str(month) + ', "E")'
-timesheets = driver.find_elements_by_class_name("statustd")
+monthstr = "jahrmonat(" + str(config.year) + "," + str(config.month) + ', "E")'
+timesheets = driver.find_elements("class name", "statustd")
 
 # search until chosen month is found
 for timesheet in timesheets:
@@ -79,7 +58,7 @@ for timesheet in timesheets:
 ndays = 31
 
 # switch to the main table frame
-frame = driver.find_element_by_name("Hauptfenster")
+frame = driver.find_element("name", "Hauptfenster")
 driver.switch_to.frame(frame)
 
 
@@ -104,35 +83,37 @@ def random_time(hour, minute, noise):
 for n in range(1, ndays + 1):
 
     # randomise start and end times
-    rand_starthr, rand_startmin = random_time(starthr, startmin, rand_start)
-    rand_endhr, rand_endmin = random_time(endhr, endmin, rand_end)
+    rand_starthr, rand_startmin = random_time(
+        config.starthr, config.startmin, config.rand_start
+    )
+    rand_endhr, rand_endmin = random_time(config.endhr, config.endmin, config.rand_end)
 
     # exception raised if holiday or month < 31 days
     try:
         # check that day is a normal working day
         type_id = "anab" + str(n)
-        type_elem = Select(driver.find_element_by_name(type_id))
+        type_elem = Select(driver.find_element("name", type_id))
         default_text = type_elem.first_selected_option.text
 
         # start hour
         strthr_id = "tf_vonSS" + str(n)
-        strthr_elem = Select(driver.find_element_by_name(strthr_id))
+        strthr_elem = Select(driver.find_element("name", strthr_id))
 
         # start minute
         strtmin_id = "tf_vonMM" + str(n)
-        strtmin_elem = Select(driver.find_element_by_name(strtmin_id))
+        strtmin_elem = Select(driver.find_element("name", strtmin_id))
 
         # end hour
         endhr_id = "tf_bisSS" + str(n)
-        endhr_elem = Select(driver.find_element_by_name(endhr_id))
+        endhr_elem = Select(driver.find_element("name", endhr_id))
 
         # end minute
         endmin_id = "tf_bisMM" + str(n)
-        endmin_elem = Select(driver.find_element_by_name(endmin_id))
+        endmin_elem = Select(driver.find_element("name", endmin_id))
 
         # input data if day is normal working day
-        autofill_types = ["not entered", "teleworking"]
-        overwrite_types = ["present", "teleworking"]
+        autofill_types = ["not entered", "mobile working"]
+        overwrite_types = ["present", "mobile working"]
 
         # if data should be filled
         if default_text in autofill_types:
@@ -146,7 +127,7 @@ for n in range(1, ndays + 1):
                 type_elem.select_by_value("9999")
 
         # if overwriting data
-        elif default_text in overwrite_types and overwrite_data:
+        elif default_text in overwrite_types and config.overwrite_data:
             strthr_elem.select_by_value(str(rand_starthr))
             strtmin_elem.select_by_value(str(rand_startmin))
             endhr_elem.select_by_value(str(rand_endhr))
@@ -159,7 +140,7 @@ for n in range(1, ndays + 1):
         break
 
 # if save and exit
-if save_and_exit:
+if config.save_and_exit:
 
     # sleep for a second
     time.sleep(1)
@@ -167,11 +148,11 @@ if save_and_exit:
     driver.switch_to.parent_frame()
 
     # switch to navigation frame
-    frame = driver.find_element_by_name("Navigation")
+    frame = driver.find_element("name", "Navigation")
     driver.switch_to.frame(frame)
 
     # click save button
-    save_button = driver.find_element_by_name("Save_az")
+    save_button = driver.find_element("name", "Save_az")
     save_button.click()
 
     # close browser
