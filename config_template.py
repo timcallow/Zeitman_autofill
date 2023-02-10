f2b5c6d2cf83343934e1b580848d28f1e144a701
Timothy Callow <timjcallow@gmail.com>
2023-02-10 09:47:51 +0100

    Add template config file

---
+++ config_template.py
@@ -0,0 +1,26 @@
+"""
+User configuration file for zeitweb
+"""
+
+month = 5  # month by number
+year = 2022
+
+# time data
+
+# average start time
+starthr = 9
+startmin = 30
+
+# average finish time
+endhr = 18
+endmin = 40
+
+# random noise (in blocks of 10 minutes)
+rand_start = 1
+rand_end = 3
+
+# whether to overwrite existing presence data
+overwrite_data = False
+
+# whether to automatically save and exit
+save_and_exit = False
